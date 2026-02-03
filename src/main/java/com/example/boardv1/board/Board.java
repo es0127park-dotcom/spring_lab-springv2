f5cd39ae4ebae53de0e02eae6cfaf1404a0aefa9
parkcoding <es0127.park@gmail.com>
2026-02-03 15:57:31 +0900

    연관관계 매핑

--- src/main/java/com/example/boardv1/board/Board.java
+++ src/main/java/com/example/boardv1/board/Board.java
@@ -4,28 +4,45 @@ import java.sql.Timestamp;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.example.boardv1.user.User;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * 데이터베이스 세상의 테이블을 자바 세상에 모델링한 결과 = 엔티티
  */
 
 @NoArgsConstructor // 디폴트 생성자
-@Data // getter, setter, toString
+@Getter
+@Setter
 @Entity // 해당 어노테이션을 보고 컴퍼넌트 스캔 후 데이터베이스에 테이블을 생성
 @Table(name = "board_tb") // 테이블명 설정
-public class Board {
+public class Board {  // user 1, Board N
     @Id // id를 primary key로 설정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment 설정
     private Integer id;
     private String title;
     private String content;
+
+    // private Integer userId;
+    @ManyToOne(fetch = FetchType.EAGER) // FK지정 / Board가 many, User가 one
+    private User user; // user_id = 1 (select * from user_tb where id = 1)
+
     @CreationTimestamp
     private Timestamp createdAt; // import 주의!! (java.sql)
+
+    @Override
+    public String toString() {
+        return "Board [id=" + id + ", title=" + title + ", content=" + content + ", user=" + user + ", createdAt="
+                + createdAt + "]";
+    }
 }
